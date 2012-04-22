ff918beeb8c394f662717416385727ec1c917d6a
Maverick Motherfucker <MMavipc@hostedyou.com>
2012-04-21 19:14:29 -0700

    Forgot files and modified gitignore

---
+++ Core/Core.cpp
@@ -0,0 +1,4 @@
+// Core.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "CoreExport.h"
---
+++ Test-Implementation/Test-Implementation.cpp
@@ -0,0 +1,7 @@
+// Test-Implementation.cpp : Defines the entry point for the console application.
+//
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
