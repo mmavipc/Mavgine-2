ae445c9ab6e309f275723b694b96441f2fb7ad74
Maverick Motherfucker <MMavipc@hostedyou.com>
2012-04-23 19:16:32 -0700

    Finshed framework for OS abstraction and added windows abstraction for MsgBox

--- Static-Shared-Lib/OSAbstraction/OSAbstraction.cpp
+++ Static-Shared-Lib/OSAbstraction/OSAbstraction.cpp
@@ -3,3 +3,32 @@
 #ifdef _WIN32
 #include <Windows.h>
 #endif
+
+Mavgine2::OSInterface* g_osi = NULL;
+
+Mavgine2::OSInterface* Mavgine2::GetOSInterface()
+{
+	if(!g_osi)
+	{
+		#ifdef _WIN32
+			g_osi = new Mavgine2::WinInterface;
+		#endif
+	}
+	return g_osi;
+}
+
+void Mavgine2::WinInterface::MsgBox(const std::string &strTitle, const std::string &strText, const MsgBoxType &msgType)
+{
+	switch(msgType)
+	{
+		case NORMAL:
+			MessageBoxA(NULL, strText.c_str(), strTitle.c_str(), MB_OK);
+			break;
+		case ALERT:
+			MessageBoxA(NULL, strText.c_str(), strTitle.c_str(), MB_ICONWARNING);
+			break;
+		case ERRORMSG:
+			MessageBoxA(NULL, strText.c_str(), strTitle.c_str(), MB_ICONERROR);
+			break;
+	}
+}
